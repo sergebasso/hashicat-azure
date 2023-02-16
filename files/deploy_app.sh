2b72bf5cc690f0bb17a8d13a4cc78529c17fb695
Serge Basso <588860+sergebasso@users.noreply.github.com>
2023-02-16 13:41:35 -0500

    Skittles x Duchess collab

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Skittles's wide world of cats. 
+  Welcome to ${PREFIX}'s app. Skittles & Duchess's wide world of cats. 
   <!-- END -->
   
   </div>
