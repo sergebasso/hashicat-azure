e3913e7f72fe15b0fa379b5f6dfc251480736c72
Serge Basso <588860+sergebasso@users.noreply.github.com>
2023-02-16 19:46:43 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/acme-corp-duchess/network/azurerm"
   version = "3.5.0"
   
-  resource_group_name = "network-rg"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
